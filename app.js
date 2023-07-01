4fd9e1b5b4fce3a31fe612c8fc9e528c219762f4
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-01 04:07:34 -0400

    added button feature

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const searchForm = document.getElementById('searchForm');
-  const searchInput = document.getElementById('searchbar');
+  let search = document.querySelector('search');
+  let submit = document.getElementById('submit');
+
+    document.querySelector('#submit').disabled = true;
+    
+    document.querySelector('#search').onkeyup = () => {
+      if(document.querySelector('#search').value.length == 0){
+        //if nothing is typed, disable the search button
+        document.querySelector('#submit').disabled = true; 
+      }
+      else{
+        document.querySelector('#submit').disabled = false;
+      }
+    }
   
   
   });
