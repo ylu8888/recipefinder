7e7da0cd5f7bc2be1f9b78be31bfcf648bfb0687
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-01 03:56:32 -0400

    added app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const searchForm = document.getElementById('searchForm');
+  const searchInput = document.getElementById('searchbar');
+  
+  
+  });
+  
+  
+  /*const searchForm = document.getElementById('searchForm');
+  const searchInput = document.getElementById('searchbar');
+  
+  
+  searchForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+  
+    const searchQuery = searchInput.value;
+  
+    window.location.href = `searchResults.html?query=${searchQuery}`;
+  });
+  */
+  
