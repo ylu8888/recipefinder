af9200273ccca9085240c1ac54f746b70c411e2c
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-01 04:54:14 -0400

    simplified JS

--- app.js
+++ app.js
@@ -2,22 +2,16 @@ document.addEventListener("DOMContentLoaded", function(){
   let search = document.querySelector('#search');
   let submit = document.querySelector('#submit');
 
-    document.querySelector('#submit').disabled = true;
+    submit.disabled = true;
     
-    document.querySelector('#search').onkeyup = () => {
-      if(document.querySelector('#search').value.length == 0){
+    search.onkeyup = () => {
+      if(search.value.length == 0){
         //if nothing is typed, disable the search button
-        document.querySelector('#submit').disabled = true; 
-    
-        //remove the hoverbackground color
-        document.querySelector('#submit').classList.remove('submit');
+        submit.disabled = true; 
 
       }
       else{
-        document.querySelector('#submit').disabled = false;
-
-        //add hover effect
-        document.querySelector('#submit').classList.add('submit');
+        submit.disabled = false;
        
       }
     }
